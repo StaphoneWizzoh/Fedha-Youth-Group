d7d4ec775a57a4c77851690eb2e1e089ac20e8c9
StaphoneWizzoh <95640951+StaphoneWizzoh@users.noreply.github.com>
2023-05-25 18:49:02 +0300

    MIME error check

--- fedha/settings.py
+++ fedha/settings.py
@@ -17,7 +17,7 @@ with open(BASE_DIR / 'fedha.json') as config_file:
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config['ALLOWED_HOSTS']
 
